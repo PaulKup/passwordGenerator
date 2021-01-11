b74943f9369118c207b213d057c222feb509316a
ChillyWilly <noreply>
2021-01-10 21:04:17 -0500

    input validation for password requirements requires at least one type of character

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,21 @@
 // Assignment code here
+var randomNum = function (min, max) {
+  return Math.random() * (max - min) + min;
+}
 
+var generatePassword = function () {
+  while (1) {
+    var lowercase = window.confirm("Would you like lowercase letters in your password?");
+    var uppercase = window.confirm("Would you like uppercase letters in your password?");
+    var numeric = window.confirm("Would you like numbers in your password?");
+    var special = window.confirm("Would you like special characters in your password?");
+
+    if (lowercase || uppercase || numeric || special) {
+      break;
+    }
+  }
+
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -7,6 +23,7 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
+  password = 'password';
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
